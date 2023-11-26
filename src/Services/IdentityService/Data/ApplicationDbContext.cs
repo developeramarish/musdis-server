51cc4222fe1971d5e357c6d52e6598940897de0a
t-eldar <teldar6077@gmail.com>
2023-11-26 23:58:19 +0300

    Changed ctor to primary constructor

--- src/Services/IdentityService/Data/ApplicationDbContext.cs
+++ src/Services/IdentityService/Data/ApplicationDbContext.cs
@@ -1,14 +1,13 @@
 ﻿using IdentityService.Models;
+
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace IdentityService.Data;
-public class ApplicationDbContext : IdentityDbContext<User>
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+    : IdentityDbContext<User>(options)
 {
-    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-        : base(options) { }
-
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
