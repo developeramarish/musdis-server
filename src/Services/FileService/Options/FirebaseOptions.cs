dd3f29b3b8779938dac1d19825fc6f06a6c3a399
t-eldar <teldar6077@gmail.com>
2023-12-07 14:25:40 +0300

    Added bucket name

--- src/Services/FileService/Options/FirebaseOptions.cs
+++ src/Services/FileService/Options/FirebaseOptions.cs
@@ -10,4 +10,5 @@ public class FirebaseOptions
     public string ProjectId { get; set; } = string.Empty;
     public string KeyEnvironmentVariableName { get; set; } = string.Empty;
     public string KeyPath { get; set; } = string.Empty;
+    public string DefaultBucketName { get; set; } = string.Empty;
 }
