8059f1419651ccda89dcc4f3e588c12998e59c35
t-eldar <teldar6077@gmail.com>
2024-01-26 16:33:45 +0300

    Added add migrations script

---
+++ scripts/add-migration.ps1
@@ -0,0 +1,16 @@
+# Scriprt for adding migraations 
+
+[CmdletBinding()]
+param (
+    [Alias("sn")]
+    [Parameter()]
+    [string]
+    $ServiceName,
+
+    [Alias("n")]
+    [Parameter()]
+    [string]
+    $Name
+)
+
+dotnet ef migrations add $Name -p .\src\Services\$ServiceName\ --msbuildprojectextensionspath .\artifacts\obj\$ServiceName\
