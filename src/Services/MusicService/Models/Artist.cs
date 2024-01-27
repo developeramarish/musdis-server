256cf00e087b7361bf0dde6f3388b97532790d9a
t-eldar <teldar6077@gmail.com>
2024-01-27 18:03:22 +0300

    Added relation properties, added CreatorId, CoverUrl properties

--- src/Services/MusicService/Models/Artist.cs
+++ src/Services/MusicService/Models/Artist.cs
@@ -1,37 +1,63 @@
 namespace Musdis.MusicService.Models;
 
+// TODO add image urls.
+
 /// <summary>
-/// Represents musicians, bands or other type of songwriters.
+///     Represents musicians, bands or other type of songwriters.
 /// </summary>
 public class Artist
 {
+    // Database rows.
+
     /// <summary>
-    /// The unique identifier of the <see cref="Artist"/>.
+    ///     The unique identifier of the <see cref="Artist"/>.
     /// </summary>
     public required Guid Id { get; set; }
 
     /// <summary>
-    /// The name of the <see cref="Artist"/>.
+    ///     The name of the <see cref="Artist"/>.
     /// </summary>
     public required string Name { get; set; }
 
     /// <summary>
-    /// The human-readable, unique identifier of the <see cref="Artist"/>.
+    ///     The human-readable, unique identifier of the <see cref="Artist"/>.
     /// </summary>
     public required string Slug { get; set; }
 
     /// <summary>
-    /// The foreign key to <see cref="Models.ArtistType"/> table.
+    ///     A URL to the cover of the <see cref="Artist"/>.
     /// </summary>
-    public required int ArtistTypeId { get; set; }
+    public required string CoverUrl { get; set; }
 
     /// <summary>
-    /// The type of the <see cref="Artist"/> (e.g. band or musician).
+    ///     The foreign key to <see cref="Models.ArtistType"/> table.
+    /// </summary>
+    public required Guid ArtistTypeId { get; set; }
+
+    /// <summary>
+    ///     The identifier of user from Identity service, who created this <see cref="Artist"/>.
+    /// </summary>
+    public required string CreatorUserId { get; set; }
+
+    // Related entities.
+    
+    /// <summary>
+    ///     The type of the <see cref="Artist"/> (e.g. band or musician).
     /// </summary>
     public ArtistType? ArtistType { get; set; }
 
     /// <summary>
-    /// Collection of <see cref="Track"/>s of this <see cref="Artist"/>.
+    ///     A collection of <see cref="Track"/>s of this <see cref="Artist"/>.
     /// </summary>
-    public IEnumerable<Track>? Tracks { get; set; }
+    public ICollection<Track>? Tracks { get; set; }
+
+    /// <summary>
+    ///     A collection of <see cref="ArtistUser"/>s of this <see cref="Artist"/>.
+    /// </summary>
+    public ICollection<ArtistUser>? ArtistUsers { get; set; }
+
+    /// <summary>
+    ///     A collection of <see cref="Release"/>s of this <see cref="Artist"/>.
+    /// </summary>
+    public ICollection<Release>? Releases { get; set; }
 }
