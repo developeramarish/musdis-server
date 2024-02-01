ddda1221d317c281ad598fd20a9cf8c4c40de3c7
t-eldar <teldar6077@gmail.com>
2024-02-01 19:53:58 +0300

    refactor: Error duplicates deleted, change to ResponseHelper package

--- src/Services/FileService/Errors/NotFoundError.cs
+++
@@ -1,6 +0,0 @@
-using Musdis.OperationResults;
-
-namespace Musdis.FileService.Errors;
-
-public sealed class NotFoundError(string description) 
-    : Error(StatusCodes.Status404NotFound, description) { }
