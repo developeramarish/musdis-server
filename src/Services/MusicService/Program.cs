49378efe35a3f7afc593f0f2edab37fe1eeec4ba
t-eldar <teldar6077@gmail.com>
2024-02-04 21:55:18 +0300

    feat: add db context and artist service to di container

--- src/Services/MusicService/Program.cs
+++ src/Services/MusicService/Program.cs
@@ -1,14 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+using Musdis.MusicService.Data;
+using Musdis.MusicService.Requests;
 using Musdis.MusicService.Services;
+using Musdis.MusicService.Services.Data;
 
 using Slugify;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+var configuration = builder.Configuration;
+var connection = configuration.GetConnectionString("MusicDbConnection");
+
+builder.Services.AddDbContext<MusicServiceDbContext>(options =>
+    options.UseNpgsql(connection)
+);
 
 builder.Services.AddTransient<ISlugHelper, SlugHelper>();
 builder.Services.AddTransient<ISlugGenerator, SlugGenerator>();
+
+builder.Services.AddTransient<IArtistService, ArtistService>();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
@@ -18,7 +34,26 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+
 app.UseHttpsRedirection();
+
+app.MapGet("/a", async (MusicServiceDbContext dbContext) =>
+{
+    return Results.Ok(await dbContext.Artists.ToListAsync());
+});
+
+app.MapPost("/artist", async (
+    CreateArtistRequest request,
+    [FromServices] IArtistService artistService
+) =>
+{
+    var result = await artistService.CreateAsync(request);
+
+    return result.IsSuccess
+        ? Results.Ok(result.Value)
+        : Results.Ok(result.Error);
+});
+
 
 app.Run();
 
