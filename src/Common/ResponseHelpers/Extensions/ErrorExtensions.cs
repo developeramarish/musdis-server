00b6a1ba16c6fd1e04f771fd2e04f38847d21037
t-eldar <teldar6077@gmail.com>
2024-02-09 15:58:38 +0300

    feat: add Error extension method for converting to HTTP Problem result

---
+++ src/Common/ResponseHelpers/Extensions/ErrorExtensions.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Http;
+
+using Musdis.OperationResults;
+using Musdis.ResponseHelpers.Errors;
+
+namespace Musdis.ResponseHelpers.Extensions;
+
+/// <summary>
+///     Extension methods for <see cref="Error"/> class.
+/// </summary>
+public static class ErrorExtensions
+{
+    /// <summary>
+    ///     Converts any type of <see cref="Error"/> into HTTP <see cref="IResult"/>.
+    /// </summary>
+    /// <remarks>
+    ///     Returns <see cref="HttpError.ToProblemHttpResult(string)"/> for errors 
+    ///     derived from <see cref="HttpError"/>. 
+    ///     Other errors converted into <see cref="InternalServerError"/> and 
+    ///     its <see cref="HttpError.ToProblemHttpResult(string)"/> returned.
+    /// </remarks>
+    /// 
+    /// <param name="error">
+    ///     An <see cref="Error"/> to convert.
+    /// </param>
+    /// 
+    /// <returns>
+    ///     <see cref="IResult"/> as problem details.
+    /// </returns>
+    public static IResult ToHttpResult(this Error error, string instance)
+    {
+        return error switch
+        {
+            HttpError httpError => httpError.ToProblemHttpResult(instance),
+            _ => new InternalServerError(error.Description).ToProblemHttpResult(instance),
+        };
+    }
+}
