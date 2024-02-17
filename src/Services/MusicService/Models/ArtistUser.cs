f87b3ec75d0bc419ea53007039b4516c99a8d766
t-eldar <teldar6077@gmail.com>
2024-02-17 13:32:56 +0300

    feat: add username info to ArtistUser model

--- src/Services/MusicService/Models/ArtistUser.cs
+++ src/Services/MusicService/Models/ArtistUser.cs
@@ -2,8 +2,10 @@ namespace Musdis.MusicService.Models;
 
 /// <summary>
 ///     Represents model that contains info about <see cref="Models.Artist"/> and associated Users 
-///     (User models specified in another service that handles authentication and authorization).
 /// </summary>
+/// <remarks>
+///     User models specified in another service that handles authentication and authorization.
+/// </remarks>
 public class ArtistUser
 {
     /// <summary>
@@ -20,4 +22,9 @@ public class ArtistUser
     ///     The user identifier within the authentication and authorization service.
     /// </summary>
     public required string UserId { get; set; }
+
+    /// <summary>
+    ///     The username of user within the authentication and authorization service.
+    /// </summary>
+    public required string UserName { get; set; }
 }
