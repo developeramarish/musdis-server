1331a1de068ea20f62f5c6bb04287a6611848cb8
t-eldar <teldar6077@gmail.com>
2024-02-17 16:06:55 +0300

    feat: add update release request validator

---
+++ src/Services/MusicService/Validation/UpdateReleaseRequestValidator.cs
@@ -0,0 +1,37 @@
+using FluentValidation;
+
+using Musdis.MusicService.Data;
+using Musdis.MusicService.Requests;
+
+namespace Musdis.MusicService.Validation;
+
+/// <summary>
+///     The validator for the <see cref="UpdateReleaseRequest"/>
+/// </summary>
+public sealed class UpdateReleaseRequestValidator : AbstractValidator<UpdateReleaseRequest>
+{
+    public UpdateReleaseRequestValidator(MusicServiceDbContext dbContext)
+    {
+
+        RuleFor(x => x.Name).NotEmpty().When(x => x.Name is not null);
+
+        RuleFor(x => x.CoverUrl).NotEmpty().When(x => x.CoverUrl is not null);
+
+        RuleFor(x => x.ReleaseDate)
+            .Must(str => RuleHelpers.BeDateString(str!))
+            .When(x => x.ReleaseDate is not null);
+
+        RuleFor(x => x.ReleaseTypeSlug)
+            .MustAsync((slug, cancel) =>
+                RuleHelpers.BeExistingReleaseTypeSlugAsync(slug!, dbContext, cancel)
+            )
+            .When(x => x.ReleaseTypeSlug is not null);
+
+        RuleFor(x => x.ArtistIds)
+            .NotEmpty()
+            .MustAsync((ids, cancel) =>
+                RuleHelpers.BeExistingArtistIdsAsync(ids!, dbContext, cancel)
+            )
+            .When(x => x.ArtistIds is not null);
+    }
+}
