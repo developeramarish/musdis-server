4a69edc8be518061abfb96f8f783ef3720fa15d0
t-eldar <teldar6077@gmail.com>
2024-02-17 16:04:29 +0300

    feat: add update release request

--- src/Services/MusicService/Requests/UpdateReleaseRequest.cs
+++ src/Services/MusicService/Requests/UpdateReleaseRequest.cs
@@ -1,3 +1,28 @@
 namespace Musdis.MusicService.Requests;
 
-public sealed record UpdateReleaseRequest();
+/// <summary>
+///     A request to update a release.
+/// </summary>
+/// 
+/// <param name="Name">
+///     A new name of the release.
+/// </param>
+/// <param name="ReleaseTypeSlug">
+///     A slug of new release type.
+/// </param>
+/// <param name="ReleaseDate">
+///     An updated release date.
+/// </param>
+/// <param name="CoverUrl">
+///     A new URL of release cover.
+/// </param>
+/// <param name="ArtistIds">
+///     An updated collection of artists of the release.
+/// </param>
+public sealed record UpdateReleaseRequest(
+    string? Name,
+    string? ReleaseTypeSlug,
+    string? ReleaseDate,
+    string? CoverUrl,
+    IEnumerable<Guid>? ArtistIds
+);
