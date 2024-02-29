f0a9c2031af479014e4b15abc4f6cb2919b416e5
t-eldar <teldar6077@gmail.com>
2024-02-29 13:31:39 +0300

    style: remove comments

--- src/Services/MusicService/Data/Configuration/TrackArtistConfiguration.cs
+++ src/Services/MusicService/Data/Configuration/TrackArtistConfiguration.cs
@@ -10,14 +10,8 @@ public sealed class TrackArtistConfiguration : IEntityTypeConfiguration<TrackArt
     public void Configure(EntityTypeBuilder<TrackArtist> builder)
     {
         builder.Property(ra => ra.ArtistId).IsRequired();
-        // builder.HasOne(ra => ra.Artist)
-        //     .WithMany()
-        //     .HasForeignKey(ra => ra.ArtistId);
 
         builder.Property(ra => ra.TrackId).IsRequired();
-        // builder.HasOne(ra => ra.Track)
-        //     .WithMany()
-        //     .HasForeignKey(ra => ra.TrackId);
 
         builder.HasKey(ra => new { ra.TrackId, ra.ArtistId });
     }
