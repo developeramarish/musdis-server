3d7339fe81f00fbb83276744429b9e38566b7d72
t-eldar <teldar6077@gmail.com>
2024-02-29 13:29:39 +0300

    feat: add new GetUserInfos method

--- src/Services/IdentityService/Services/Grpc/UserGrpcService.cs
+++ src/Services/IdentityService/Services/Grpc/UserGrpcService.cs
@@ -54,12 +54,62 @@ public sealed class UserGrpcService : UserService.UserServiceBase
             _logger.LogError("Cannot find User with Id = {Id}", request.Id);
             throw new RpcException(new Status(
                 StatusCode.NotFound,
-                $"User with Id = {request.Id} is not found."
+                $"User with Id = {{{request.Id}}} is not found."
             ));
         }
 
         _logger.LogInformation("Retrieving user data with Id = {Id}", user.Id);
-        
+
         return new() { Id = user.Id, UserName = user.UserName };
+    }
+
+    public override async Task<UserInfos> GetUserInfos(
+        GetUserInfosRequest request,
+        ServerCallContext context
+    )
+    {
+        if (request.Ids.Count == 0)
+        {
+            throw new RpcException(new Status(
+                StatusCode.InvalidArgument,
+                "Identifiers list cannot be empty"
+            ));
+        }
+
+        var users = await _dbContext.Users
+            .AsNoTracking()
+            .Where(u => request.Ids.Contains(u.Id))
+            .ToArrayAsync(context.CancellationToken);
+
+        if (users.Length != request.Ids.Distinct().Count())
+        {
+            var invalidIds = string.Join(", ", request.Ids
+                .Distinct()
+                .Except(users.Select(u => u.Id))
+            );
+            _logger.LogError(
+                "Cannot find Users with Ids = [{Ids}]",
+                invalidIds
+            );
+
+            throw new RpcException(new Status(
+                StatusCode.NotFound,
+                $"Users with Ids = [{invalidIds}] are not found."
+            ));
+        }
+
+        _logger.LogInformation(
+            "Retrieving users data with Ids = {Ids}",
+            users.Select(u => u.Id)
+        );
+
+        var result = new UserInfos();
+        result.Users.AddRange(users.Select(u => new UserInfo
+        {
+            Id = u.Id,
+            UserName = u.UserName
+        }));
+
+        return result;
     }
 }
