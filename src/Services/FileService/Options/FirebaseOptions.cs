f3ee23ba839ef75a2b921f91741f3eb5975f3038
t-eldar <teldar6077@gmail.com>
2024-03-06 12:26:25 +0300

    docs: add doc comments to FirebaseOption

--- src/Services/FileService/Options/FirebaseOptions.cs
+++ src/Services/FileService/Options/FirebaseOptions.cs
@@ -5,10 +5,28 @@ namespace Musdis.FileService.Options;
 /// </summary>
 public class FirebaseOptions
 {
+    /// <summary>
+    ///     The configuration path of the Firebase option.
+    /// </summary>
     public const string Firebase = nameof(Firebase);
 
+    /// <summary>
+    ///     The project id of the Firebase project.
+    /// </summary>
     public string ProjectId { get; set; } = string.Empty;
+
+    /// <summary>
+    ///     The key environment variable name of the Firebase project.
+    /// </summary>
     public string KeyEnvironmentVariableName { get; set; } = string.Empty;
+
+    /// <summary>
+    ///     The path to the Firebase project credentials.
+    /// </summary>
     public string KeyPath { get; set; } = string.Empty;
+
+    /// <summary>
+    ///     The default bucket name of the Firebase project.
+    /// </summary>
     public string DefaultBucketName { get; set; } = string.Empty;
 }
