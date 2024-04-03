32b1ea89f11c9c76b1c349f7c85ce98523540e34
t-eldar <teldar6077@gmail.com>
2024-04-03 14:34:41 +0300

    feat: add authentication, add antiforgery endpoint

--- src/Services/FileService/Program.cs
+++ src/Services/FileService/Program.cs
@@ -4,8 +4,27 @@ using Musdis.FileService.Services.Storage;
 using Google.Cloud.Storage.V1;
 using Musdis.FileService.Data;
 using Microsoft.EntityFrameworkCore;
+using Musdis.FileService.Endpoints;
+using Microsoft.AspNetCore.Antiforgery;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.OpenApi.Models;
+using Musdis.FileService.Swagger;
+using Musdis.OperationResults;
+using Musdis.ResponseHelpers.Extensions;
+using Musdis.AuthHelpers.Extensions;
+using FluentValidation;
+using Musdis.FileService.Validation;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCommonAuthentication(
+    builder.Configuration,
+    "JwtSettings"
+);
+builder.Services.AddAuthorization();
 
 builder.Services.AddDbContext<FileServiceDbContext>(options =>
 {
@@ -13,6 +32,7 @@ builder.Services.AddDbContext<FileServiceDbContext>(options =>
         ?? throw new InvalidOperationException("Database connection is missing.");
     options.UseNpgsql(connection);
 });
+builder.Services.AddAntiforgery(options => options.HeaderName = "X-XSRF-TOKEN");
 
 // Options
 builder.Services.AddOptions<FirebaseOptions>()
@@ -21,18 +41,25 @@ builder.Services.AddOptions<FirebaseOptions>()
 
 // Environment variables
 Environment.SetEnvironmentVariable(
-    builder.Configuration["Firebase:KeyEnvironmentVariableName"] 
+    builder.Configuration["Firebase:KeyEnvironmentVariableName"]
         ?? throw new InvalidOperationException("Cannot start app, configuration is missing Firebase:KeyEnvironmentVariableName"),
     builder.Configuration["Firebase:KeyPath"]
         ?? throw new InvalidOperationException("Cannot start app, configuration is missing Firebase:KeyPath")
 );
+ValidatorOptions.Global.LanguageManager.Enabled = false;
+builder.Services.AddScoped<IValidator<IFormFile>, FormFileValidator>();
 
 builder.Services.AddTransient(_ => StorageClient.Create());
 builder.Services.AddTransient<IStorageProvider, FirebaseStorageProvider>();
 
-builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo { Title = "FileService", Version = "v1" });
+    options.OperationFilter<XsrfHeaderFilter>();
+    options.CustomSchemaIds(type => type.ToString());
+    options.AddJwtAuthorization();
+});
 
 var app = builder.Build();
 
@@ -42,8 +69,35 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseAuthentication();
+app.UseAuthorization();
+
 app.UseHttpsRedirection();
 
-app.MapControllers();
+app.MapGet("antiforgery/token", (
+    HttpContext context,
+    [FromServices] IAntiforgery antiforgery
+) =>
+{
+    var tokens = antiforgery.GetAndStoreTokens(context);
+    if (tokens.RequestToken is null)
+    {
+        return new Error(
+            "Unable to generate antiforgery token, request token is missing."
+        ).ToHttpResult(context.Request.Path);
+    }
+    context.Response.Cookies.Append(
+        "XSRF-TOKEN",
+        tokens.RequestToken,
+        new CookieOptions { HttpOnly = false }
+    );
+
+    return Results.Ok();
+}).RequireAuthorization();
+
+app.MapGroup("files").MapFiles();
+
+app.UseAntiforgery();
+
 
 app.Run();
