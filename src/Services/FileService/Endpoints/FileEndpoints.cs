23f59f3875992c9949f412bc545254ede24bca8f
t-eldar <teldar6077@gmail.com>
2024-04-03 14:39:32 +0300

    refactor: change endpoints and their handlers

--- src/Services/FileService/Endpoints/FileEndpoints.cs
+++ src/Services/FileService/Endpoints/FileEndpoints.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 
 using Musdis.FileService.Services.Storage;
 using Musdis.ResponseHelpers.Extensions;
+using Musdis.ResponseHelpers.Responses;
 
 namespace Musdis.FileService.Endpoints;
 
@@ -21,12 +22,62 @@ public static class FileEndpoints
     /// <returns>
     ///     Route group with mapped endpoints.
     /// </returns>
-    public static RouteGroupBuilder MapSomething(
+    public static RouteGroupBuilder MapFiles(
         this RouteGroupBuilder groupBuilder
     )
     {
-        // TODO add endpoints
-    
+        groupBuilder.MapPost("/", HandlePostFileAsync).RequireAuthorization();
+
+        groupBuilder.MapGet("/", HandleGetFileUrlsAsync);
+
         return groupBuilder;
     }
+
+    public static async Task<IResult> HandlePostFileAsync(
+        [FromForm] IFormFile file,
+        [FromServices] IStorageService storageService,
+        HttpContext context,
+        CancellationToken cancellationToken
+    )
+    {
+        var result = await storageService.UploadFileAsync(file, cancellationToken);
+        if (result.IsFailure)
+        {
+            return result.Error.ToHttpResult(context.Request.Path);
+        }
+
+        return Results.Ok(result.Value);
+    }
+
+    public static async Task<IResult> HandleGetFileUrlsAsync(
+        [FromQuery] string[] filePaths,
+        [FromServices] IStorageProvider storageProvider,
+        HttpContext context,
+        CancellationToken cancellationToken
+    )
+    {
+        var result = await storageProvider.GetFileUrlsAsync(filePaths, cancellationToken);
+        if (result.IsFailure)
+        {
+            return result.Error.ToHttpResult(context.Request.Path);
+        }
+
+        return Results.Ok(new DataResponse<IList<Uri>>(result.Value));
+    }
+
+    // public static async Task<IResult> HandleGetFileUrlAsync(
+    //     [FromQuery] string filePath,
+    //     [FromServices] IStorageProvider storageProvider,
+    //     HttpContext context,
+    //     CancellationToken cancellationToken
+    // )
+    // {
+    //     var result = await storageProvider.GetFileUrlAsync(filePath, cancellationToken);
+    //     if (result.IsFailure)
+    //     {
+    //         return result.Error.ToHttpResult(context.Request.Path);
+    //     }
+
+    //     return Results.Ok(new DataResponse<Uri>(result.Value));
+    // }
 }
