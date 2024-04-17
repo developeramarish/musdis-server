09579b6e73a79f541787f46a7ab220cd170ec1be
t-eldar <teldar6077@gmail.com>
2024-04-17 17:03:43 +0300

    feat: add exception handler

---
+++ src/Services/FileService/Services/Exceptions/ExceptionHandler.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+using Musdis.ResponseHelpers.Errors;
+
+namespace Musdis.FileService.Services.Exceptions;
+
+/// <summary>
+///     The global application exception handler.
+/// </summary>
+public class ExceptionHandler : IExceptionHandler
+{
+    private readonly ILogger<ExceptionHandler> _logger;
+
+    public ExceptionHandler(ILogger<ExceptionHandler> logger)
+    {
+        _logger = logger;
+    }
+
+    public async ValueTask<bool> TryHandleAsync(
+        HttpContext httpContext, 
+        Exception exception,
+        CancellationToken cancellationToken
+    )
+    {
+        _logger.LogError(exception, "Exception occurred. Details: {Error}", exception.Message);
+
+        var problemDetails = new ProblemDetails
+        {
+            Status = StatusCodes.Status500InternalServerError,
+            Title = "Internal server error",
+            Instance = httpContext.Request.Path,
+            Type = InternalServerError.ProblemDetailsType,
+        };
+
+        httpContext.Response.StatusCode = problemDetails.Status.Value;
+
+        await httpContext.Response.WriteAsJsonAsync(problemDetails, cancellationToken);
+
+        return true;
+    }
+}
